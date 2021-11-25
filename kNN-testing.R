64e0505d2b4389fc92d7e5cf2d48dfe329696942
fannif <43149217+fannif@users.noreply.github.com>
2021-11-25 14:26:05 +0200

    Clearing extra lines

--- kNN-testing.R
+++ kNN-testing.R
@@ -26,7 +26,7 @@ test_mean <- npfm[-idx,]
 # Now [,"class4"] gives y
 # and [,-1] should give X
 
-trctrl <- trainControl(method = "repeatedcv", number = 10, repeats = 3)
+trctrl <- trainControl(method = "repeatedcv", number = 10, repeats = 10, classProbs=T)
 knn_all <- train(class4 ~., data = train_all, method = "knn",
                  trControl=trctrl,
                  preProcess = c("center", "scale"),
@@ -41,20 +41,4 @@ mean_pred <- predict(knn_mean, newdata = test_mean)
 
 conf_all <- confusionMatrix(all_pred, test_all$class4)
 conf_mean <- confusionMatrix(mean_pred, test_mean$class4)
-
-#rmse <- function(yhat,y) sqrt(mean((y-yhat)**2))
-
-#knn_all_rmse <- rmse(all_pred, test_all$class4)
-#knn_mean_rmse <- rmse(mean_pred, test_all$class4)
-
-#knn_all <- knnreg(class4 ~ .,data=train_all, k=5)
-#knn_mean <- knnreg(class4 ~ .,data=train_mean, k=5)
-
-#knn_all_y <- predict(knn_all, newdata = test_all)
-#knn_mean_y <- predict(knn_mean, newdata = test_mean)
-
-#knn_all_rmse <- rmse(knn_all_y, test_all$class4)
-#knn_mean_rmse <- rmse(knn_mean_y, test_all$class4)
-
-
 
